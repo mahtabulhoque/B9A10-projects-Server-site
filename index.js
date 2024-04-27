4f1d83a7bdca4c506d9fe39db488ae4362cbc495
hagghat <mahach0396@gmail.com>
2024-04-27 23:04:01 +0600

    4th

--- index.js
+++ index.js
@@ -27,12 +27,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const newCollection = client.db('addCraftItemDB').collection('addCraftItem');
+
+    app.get('/addCraftItem', async(req,res) =>{
+      const cursor = newCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
+
+    app.post('/addCraftItem', async(req,res)=>{
+      const newItem = req.body;
+      console.log(newItem);
+      const result = await newCollection.insertOne(newItem);
+      res.send(result)
+
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
